e273c9d1aafe4d10604da0f9c75ba055b05823a2
kiddopro <ms.prgramador@gmail.com>
2024-09-19 15:23:02 -0300

    almost finished

--- data/products.js
+++ data/products.js
@@ -6,7 +6,21 @@ const products = {
         title: 'watch1',
         desc: '',
         price: 0,
-        url: []
+        // url: []
+        img: [],
+        vat: 0,
+        rate: 0,
+        stock: 0,
+        comments: [
+          {
+            id: 1,
+            user: "",
+            fullname: "",
+            email: "",
+            photo: "",
+            comment: "",
+          }
+        ]
       }
     ]
   },
--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const homeRoutes = require("./routes/home");
 const productRoutes = require("./routes/product");
 
 app.use(express.json());
 app.use(express.static('public'))
 
 // Usar las rutas
-//
-app.use('/api', homeRoutes);
 app.use('/api', productRoutes);
 
 app.listen(port, () => {
